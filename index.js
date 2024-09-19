058247e3880d63fea6387e8ef13ed8963744034a
bandreae <brandonjandreae@gmail.com>
2024-09-18 23:52:06 -0500

    got the mysql connection working and testing on the index.js file.

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const app = express()
 const employees = require("./routes/employees")
+const database = require("./mysql/connection")
 
 const port = process.env.PORT || 4001;
 
 app.use(employees)
+app.get(database)
 
 app.get('/', (req, res) => {
-    res.send('Welcome to our API')
-  })
+  // aControllerFile.js
+  database.execute('SELECT * FROM employees', (err, rows) => {
+    if (err) {
+      console.log({ 'message': 'Error occurred: ' + err })
+      return res.status(500).send('An unexpected error occurred')
+    }
+    res.json(rows)
+  });
+  // res.send('Welcome to our API')
+})
 
 app.listen(port, () => {
-    console.log(`Web server is listening on port ${port}!`);
-   });
+  console.log(`Web server is listening on port ${port}!`);
+});
