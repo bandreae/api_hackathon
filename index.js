2bd2efd97ee06c4cfa5af8d40adc25cc3b859a9b
cesarOchoa90 <hire.cesar@outlook.com>
2024-09-22 17:27:29 -0500

    3 routes for salaries: all salaries, salary by id, and salary by first name

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const express = require('express')
 const app = express()
 const employees = require("./routes/employees")
+const salaries = require("./routes/salaries")
 const database = require("./mysql/connection")
 
 const port = process.env.PORT || 4001;
 
 app.use(employees)
+app.use(salaries)
 app.get(database)
 
-app.get('/', (req, res) => {
-  // aControllerFile.js
-  database.execute('SELECT * FROM employees', (err, rows) => {
-    if (err) {
-      console.log({ 'message': 'Error occurred: ' + err })
-      return res.status(500).send('An unexpected error occurred')
-    }
-    res.json(rows)
-  });
-  // res.send('Welcome to our API')
-})
+// app.get('/', (req, res) => {
+//   // aControllerFile.js
+//   database.execute('SELECT * FROM employees', (err, rows) => {
+//     if (err) {
+//       console.log({ 'message': 'Error occurred: ' + err })
+//       return res.status(500).send('An unexpected error occurred')
+//     }
+//     res.json(rows)
+//   });
+//   // res.send('Welcome to our API')
+// })
 
 app.listen(port, () => {
   console.log(`Web server is listening on port ${port}!`);
